b32721e4dda2774d76e68a8a6c5803b3055c58e4
Reid Sata <rsata@rubiconnt.localnet>
2016-09-29 13:55:08 -0700

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple google sheets as db
+
+## Setup
+
+Add a `creds.js` file with keys and sheet. For example:
+```var sheetsId = 'YOUR GOOGLE SHEETS ID';
+var key = 'YOUR KEY'
+module.exports = { sheetsId, key } ```
+
+Also `npm install`
+
+## Run
+
+```node app.js```
---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var axios = require('axios');
+var creds = require('./creds')
+
+var sheetsId = creds.sheetsId;
+var key = '?key=' + creds.key;
+var sheet = '/values/Sheet1!';
+var range = 'A:Z';
+
+app.get('/googlesheets', function(req, res, next) {
+  axios.get('https://sheets.googleapis.com/v4/spreadsheets/'
+    + sheetsId 
+    + sheet 
+    + range 
+    + key)
+    .then(function(resp) {
+      return resp.data.values;
+    })
+    .then(data => res.send(data))
+    .catch(function(error) {console.log(error)})
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
